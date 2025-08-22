57dff1c52a4b5f5e035cfab716d185b34b4b0d4b
Yi-Jyun Pan <me@pan93.com>
2025-08-22 23:15:07 +0800

    feat: optimize Next.js server configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    useCache: true,
+    reactCompiler: true,
+  }
 };
 
 export default nextConfig;
