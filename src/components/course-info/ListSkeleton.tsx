ca5cb03fdaa8432e823c25bb7eed966cbe21eaeb
ttymayor <lionhuang914903@gmail.com>
2025-08-23 23:07:54 +0800

    refactor(course-info): update ListSkeleton component layout

--- src/components/course-info/ListSkeleton.tsx
+++ src/components/course-info/ListSkeleton.tsx
@@ -18,21 +18,15 @@ export default function ListSkeleton({ pageSize = 10 }: ListSkeletonProps) {
       <Table className="min-w-full">
         <TableHeader>
           <TableRow className="h-12">
-            <TableHead className="text-center">課程代碼</TableHead>
-            <TableHead className="text-center">課程名稱</TableHead>
-            <TableHead className="text-center">學分</TableHead>
-            <TableHead className="text-center">教師</TableHead>
-            <TableHead className="text-center">時間地點</TableHead>
-            <TableHead className="text-center">
-              系所名稱 / 上課年級
-            </TableHead>
+            <TableHead className="text-center">操作</TableHead>
+            <TableHead className="">課程</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {Array.from({ length: pageSize }).map((_, idx) => (
             <TableRow key={`skeleton-${idx}`} className="h-12">
               <TableCell className="text-center">
-                <Skeleton className="h-4 w-12 mx-auto" />
+                <Skeleton className="h-4 w-24 mx-auto" />
               </TableCell>
               <TableCell className="text-center">
                 <Skeleton className="h-4 w-32 mx-auto" />
