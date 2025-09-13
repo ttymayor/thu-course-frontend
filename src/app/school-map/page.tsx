04fa69b4981ccce79bf6f5d74661d7b754ef678c
ttymayor <lionhuang914903@gmail.com>
2025-09-13 21:14:04 +0800

    feat(school-map): refactor SchoolMap component and update metadata description

--- src/app/school-map/page.tsx
+++ src/app/school-map/page.tsx
@@ -1,10 +1,9 @@
-import dynamic from "next/dynamic";
 import type { Metadata } from "next";
-
-const SchoolMap = dynamic(() => import("./SchoolMap"));
+import SchoolMap from "../../components/school-map/SchoolMap";
 
 export const metadata: Metadata = {
   title: "校園地圖",
+  description: "你會迷路嗎？來看看東海大學的校園地圖吧",
 };
 
 export default function SchoolMapPage() {
