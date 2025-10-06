c9c5f6f044ae24fe43b4adabf131f84ed59d0f2b
ttymayor <lionhuang914903@gmail.com>
2025-10-06 21:50:14 +0800

    refactor(schedule-simulator): restructure ScheduleSimulatorSkeleton layout with Frame component for improved UI

--- src/components/schedule-simulator/ScheduleSimulatorSkeleton.tsx
+++ src/components/schedule-simulator/ScheduleSimulatorSkeleton.tsx
@@ -1,11 +1,16 @@
 import CourseListSkeleton from "@/components/schedule-simulator/CourseListSkeleton";
 import ScheduleTable from "@/components/schedule-simulator/ScheduleTable";
+import Frame from "@/components/schedule-simulator/Frame";
 
 export default function ScheduleSimulatorSkeleton() {
   return (
-    <div className="flex flex-col gap-4">
-      <CourseListSkeleton />
-      <ScheduleTable selectedCourses={[]} />
-    </div>
+    <Frame>
+      <div className="md:w-1/3 w-full md:pr-4 min-w-0">
+        <CourseListSkeleton />
+      </div>
+      <div className="md:w-2/3 w-full min-w-0">
+        <ScheduleTable selectedCourses={[]} />
+      </div>
+    </Frame>
   );
 }
