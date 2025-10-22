c539bceab9c2be2559cce824e8ab485859c29d8b
ttymayor <lionhuang914903@gmail.com>
2025-10-22 21:43:44 +0800

    chore(next.config): enable reactCompiler option in the main configuration

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
   experimental: {
     useCache: true,
-    reactCompiler: true,
   }
 };
 
