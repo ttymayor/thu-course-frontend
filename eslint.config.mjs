72a6f313308c096bb976e14d38a2d3308a959f56
ttymayor <lionhuang914903@gmail.com>
2025-11-29 03:21:59 +0800

    chore(eslint): refactor ESLint configuration to use next-core-web-vitals and next-typescript imports; remove FlatCompat dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,13 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [{
   ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, ...compat.extends("next/core-web-vitals", "next/typescript")];
+}, ...nextCoreWebVitals, ...nextTypescript];
 
 export default eslintConfig;
